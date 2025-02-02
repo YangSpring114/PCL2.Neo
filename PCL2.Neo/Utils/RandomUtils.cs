04b646fff4af5995150f825e150123f0ca2718ef
litwak913 <litwak913@qq.com>
2025-02-02 21:48:27 +0800

    feat(utils): add random

---
+++ PCL2.Neo/Utils/RandomUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PCL2.Neo.Utils;
+
+public static class RandomUtils
+{
+    private static readonly Random random = new Random();
+
+    public static T RandomOne<T>(ICollection<T> objects)
+    {
+        return objects.ElementAt(RandomInteger(0, objects.Count - 1));
+    }
+
+    public static int RandomInteger(int min,int max)
+    {
+        return (int) Math.Floor((max - min + 1) * random.NextDouble()) + min;
+    }
+
+    public static IList<T> Shuffle<T>(IList<T> array) { 
+        IList<T> result = new List<T>();
+        do
+        {
+            int i = RandomInteger(0, array.Count - 1);
+            result.Add(array[i]);
+            array.RemoveAt(i);
+        }
+        while (array.Any());
+        return result;
+    }
+}
