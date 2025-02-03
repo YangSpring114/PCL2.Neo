8fbce3ca5b84a7f434670b5b9dad515fe30a098b
litwak913 <litwak913@qq.com>
2025-02-03 22:19:18 +0800

    docs(utils): add random doc

--- PCL2.Neo/Utils/RandomUtils.cs
+++ PCL2.Neo/Utils/RandomUtils.cs
@@ -8,16 +8,25 @@ public static class RandomUtils
 {
     private static readonly Random random = new Random();
 
+    /// <summary>
+    /// 随机选择其一。
+    /// </summary>
     public static T RandomOne<T>(ICollection<T> objects)
     {
         return objects.ElementAt(RandomInteger(0, objects.Count - 1));
     }
 
+    /// <summary>
+    /// 取随机整数。
+    /// </summary>
     public static int RandomInteger(int min,int max)
     {
         return (int) Math.Floor((max - min + 1) * random.NextDouble()) + min;
     }
 
+    /// <summary>
+    /// 将数组随机打乱。
+    /// </summary>
     public static IList<T> Shuffle<T>(IList<T> array) { 
         IList<T> result = new List<T>();
         do
