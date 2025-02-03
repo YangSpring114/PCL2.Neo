fb94da21f8125f6c02f591a39fab06b6d42962bd
litwak913 <litwak913@qq.com>
2025-02-03 23:12:25 +0800

    feat(utils): add LoadState and Result enum

--- PCL2.Neo/Utils/CoreUtils.cs
+++ PCL2.Neo/Utils/CoreUtils.cs
@@ -27,4 +27,46 @@ public static class CoreUtils
     /// 指示用户手动取消了操作，或用户已知晓操作被取消的原因。
     /// </summary>
     public class CancelledException : Exception;
+    
+    public enum LoadState
+    {
+        Waiting,
+        Loading,
+        Finished,
+        Failed,
+        Aborted,
+    }
+
+    public enum Result
+    {
+        /// <summary>
+        /// 执行成功，或进程被中断。
+        /// </summary>
+        Aborted = -1,
+
+        /// <summary>
+        /// 执行成功。
+        /// </summary>
+        Success = 0,
+
+        /// <summary>
+        /// 执行失败。
+        /// </summary>
+        Fail,
+
+        /// <summary>
+        /// 执行时出现未经处理的异常。
+        /// </summary>
+        Exception,
+
+        /// <summary>
+        /// 执行超时。
+        /// </summary>
+        Timeout,
+
+        /// <summary>
+        /// 取消执行。可能是由于不满足执行的前置条件。
+        /// </summary>
+        Cancel
+    }
 }
