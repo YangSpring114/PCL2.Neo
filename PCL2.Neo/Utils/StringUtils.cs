ee06dabb06831a8b85bf0b95d5315b79a277a7c6
litwak913 <litwak913@qq.com>
2025-02-03 21:36:58 +0800

    feat(utils): add GetStringFromEnum and ContainsF extension

---
+++ PCL2.Neo/Utils/StringUtils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace PCL2.Neo.Utils;
+
+public static class StringUtils
+{
+    /// <summary>
+    /// 返回一个枚举对应的字符串。
+    /// </summary>
+    public static string GetStringFromEnum(Enum enumData)
+    {
+        return Enum.GetName(enumData.GetType(), enumData);
+    }
+
+    /// <summary>
+    /// 支持可变大小写判断的 Contains。
+    /// </summary>
+    public static bool ContainsF(this string str,string subStr,bool ignoreCase = false)
+    {
+        return str.IndexOf(subStr, (ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal)) >= 0;
+    }
+}
